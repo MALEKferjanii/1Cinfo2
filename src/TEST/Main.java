dbea8807c30056e903580cc3b39f8b237253ec0f
MALEKferjanii <ferjenimelek42@gmail.com>
2024-05-03 19:13:06 +0100

    first commit

---
+++ src/TEST/Main.java
@@ -0,0 +1,16 @@
+package TEST;
+
+import static TEST.methodes.calculerSomme;
+
+public class Main {
+    public static void main(String[] args) {
+// Test des méthodes
+        int[] tableau = {1, 2, 3, 4, 5};
+        System.out.println("Somme du tableau : " + calculerSomme(tableau));
+//        System.out.println("Moyenne du tableau : " + calculerMoyenne(tableau));
+//        System.out.println("Factorielle de 5 : " + calculerFactorielle(5));
+//        System.out.println("Est-ce que 6 est pair ? " + estPair(6));
+//        System.out.println("Est-ce que 7 est premier ? " + estPremier(7));
+//        System.out.println("Inverser la chaîne 'hello' : " + inverserChaine("hello"));
+    }
+}
